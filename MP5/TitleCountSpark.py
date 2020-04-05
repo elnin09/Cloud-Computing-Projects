8c6e8bdeddc56ca7296dafb4dfb0c57738b9ffae
Swapnil Darmora <darmora2@illinois.edu>
2020-04-05 20:29:10 +0530

    Adding changes for MP5

--- MP5/TitleCountSpark.py
+++ MP5/TitleCountSpark.py
@@ -4,14 +4,19 @@
 
 import sys
 from pyspark import SparkConf, SparkContext
+import re
 
 stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
+delimiters = ""
 
 with open(stopWordsPath) as f:
+    data = f.read()
+    stopwords = re.split("\\n|\\s",data)
 	#TODO
 
 with open(delimitersPath) as f:
+    delimiters=",|;|\.|\?|!|-|:|@|\[|\]|\(|\)|\{|\}|_|\*|\/|\\n| "
     #TODO
 
 conf = SparkConf().setMaster("local").setAppName("TitleCount")
@@ -19,6 +24,22 @@ conf.set("spark.driver.bindAddress", "127.0.0.1")
 sc = SparkContext(conf = conf)
 
 lines = sc.textFile(sys.argv[3],1)
+
+
+def titlecountmap(line):
+    retval = list()
+    words=re.split(delimiters,line.lower());
+    for word in words:
+        if word not in stopwords and word != '':
+            retval.append((word,1))
+    
+
+
+
+wc = lines.flatMap(lambda x: titlecountmap(x))
+
+reduce = wc.reduceByKey(lambda a, b: a + b)
+
 
 #TODO
 
