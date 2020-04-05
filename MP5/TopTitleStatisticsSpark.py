b72e72daa5772bd10ab5c0c2f37150cf24c8aed4
Swapnil Darmora <darmora2@illinois.edu>
2020-04-06 00:19:15 +0530

    Adding changes for MP5

--- MP5/TopTitleStatisticsSpark.py
+++ MP5/TopTitleStatisticsSpark.py
@@ -10,16 +10,27 @@ lines = sc.textFile(sys.argv[1],1)
 
 #TODO
 
+def mymap(x):
+    mapoutputkey,mapoutputcount = x.split('\t', 1)
+    return (mapoutputkey,mapoutputcount)
+
+wc = lines.map(mymap)
+
 outputFile = open(sys.argv[2],"w")
-'''
-TODO write your output here
-write results to output file. Format
+
+#TODO write your output here
+#write results to output file. Format
+ans1 = wc.sum()/10
 outputFile.write('Mean\t%s\n' % ans1)
+ans2 = wc.sum()
 outputFile.write('Sum\t%s\n' % ans2)
+ans3 = wc.min()
 outputFile.write('Min\t%s\n' % ans3)
+ans4 = wc.max()
 outputFile.write('Max\t%s\n' % ans4)
+ans5 = wc.var()
 outputFile.write('Var\t%s\n' % ans5)
-'''
+
 
 sc.stop()
 
