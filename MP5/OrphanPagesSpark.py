27f3d0f4d47cdb62f7f2ee280701cd2a71492f05
Swapnil Darmora <darmora2@illinois.edu>
2020-04-06 01:55:25 +0530

    Adding changes for MP5

--- MP5/OrphanPagesSpark.py
+++ MP5/OrphanPagesSpark.py
@@ -22,7 +22,8 @@ def mapperfunction(line):
         retval.append((i,key.rstrip('\n')))
     return retval
     
-def mapfunction(key,value):
+def mapfunction(x):
+    key,value = x
     retval = list()
     retval.append((key,1))
     retval.append((value,0))
@@ -35,7 +36,7 @@ print("Luada 1")
 wcflatmap = lines.flatMap(lambda x:mapperfunction(x))
 print(wcflatmap.take(10))
 print("Lauda 2")
-wc = wcflatmap.flatMap(lambda a, b: mapfunction(a,b));
+wc = wcflatmap.flatMap(lambda x: mapfunction(x));
 print(wc.take(30))
 print("lauda3")
 wcreduce = wc.reduceByKey(lambda a, b: a + b)
