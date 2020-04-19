dbc9ff3fc9716c600f5ce896c1eb1ce7e7797723
Swapnil Darmora <darmora2@illinois.edu>
2020-04-20 01:12:18 +0530

    adding changes to MP6

--- MP6/target/classes/resources/top_n_finder_bolt.py
+++ MP6/target/classes/resources/top_n_finder_bolt.py
@@ -35,7 +35,7 @@ class TopNFinderBolt(storm.BasicBolt):
         self._len +=1
 
         if self._len>10:
-            key_to_delete = max(self._topN, key=lambda k: self._topN[k])
+            key_to_delete = min(self._topN, key=lambda k: self._topN[k])
             del self._topN[key_to_delete]
         
         outtopN = "topN"
