497465f9e3c11667a06200ea8009c3e5bf79cf1e
Swapnil Darmora <darmora2@illinois.edu>
2020-04-20 02:05:18 +0530

    adding changes to MP6

--- MP6/target/classes/resources/normalizer_bolt.py
+++ MP6/target/classes/resources/normalizer_bolt.py
@@ -26,7 +26,7 @@ class NormalizerBolt(storm.BasicBolt):
         line = tup.values[0]
         words = re.split("[^a-zA-Z0-9-]",line)
         for word in words:
-            if word not in self._common_words:
+            if word not in self._common_words and word != " ":
                 word = word.lower()
                 storm.emit([word])
 
