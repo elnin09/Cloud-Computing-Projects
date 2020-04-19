04e7881e317528acfea010e59daa63c304fc76eb
Swapnil Darmora <darmora2@illinois.edu>
2020-04-19 19:29:47 +0530

    adding changes to MP6

--- MP6/multilang/resources/split_sentence_bolt.py
+++ MP6/multilang/resources/split_sentence_bolt.py
@@ -15,7 +15,7 @@ class SplitBolt(storm.BasicBolt):
         # TODO
         # Task: split sentence and emit words
         # Hint: split on "[^a-zA-Z0-9-]"
-        words = tup.values[0].split("[^a-zA-Z0-9-]")
+        words = tup.values[0].split()
         for word in words:
             storm.logInfo("Emitting %s" % word)
             storm.emit([word])
