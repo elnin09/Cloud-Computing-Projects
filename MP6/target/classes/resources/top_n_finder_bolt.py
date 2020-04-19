1d640d9567ee260f4aa6e62125d6f9c98622561b
Swapnil Darmora <darmora2@illinois.edu>
2020-04-20 01:48:40 +0530

    adding changes to MP6

--- MP6/target/classes/resources/top_n_finder_bolt.py
+++ MP6/target/classes/resources/top_n_finder_bolt.py
@@ -39,15 +39,15 @@ class TopNFinderBolt(storm.BasicBolt):
             key_to_delete = min(self._topN.keys(), key=lambda k: self._topN[k])
             del self._topN[key_to_delete]
         
-        outtopN = "topN"
-        values= ""
+        topN = "top-N"
+        partDTopN= ""
 
         for i in self._topN.keys():
             values = values + "," + i
          
         storm.logInfo("betichod") 
-        storm.logInfo("Emitting %s:%s" % (outtopN, values))
-        storm.emit([outtopN,values])
+        storm.logInfo("Emitting %s:%s" % (topN, values))
+        storm.emit([topN,values])
 
         pass
         # End
