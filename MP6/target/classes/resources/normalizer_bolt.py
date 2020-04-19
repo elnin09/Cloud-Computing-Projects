e3f12a13b919312ceea33f03e9a37251b47c3ce6
Swapnil Darmora <darmora2@illinois.edu>
2020-04-19 23:08:26 +0530

    adding changes to MP6

--- MP6/target/classes/resources/normalizer_bolt.py
+++ MP6/target/classes/resources/normalizer_bolt.py
@@ -23,10 +23,10 @@ class NormalizerBolt(storm.BasicBolt):
         # TODO:
         # Task 1: make the words all lower case
         # Task 2: remove the common words
-        line = tup.values[0]
+        line = str(tup.values[0])
+        line = line.lower()
         words = re.split("[^a-zA-Z0-9-]",line)
         for word in words and word not in self._common_words:
-            word = str((str(word)).lower())
             storm.emit([word])
 
             #storm.logInfo("Emitting %s" % word)
