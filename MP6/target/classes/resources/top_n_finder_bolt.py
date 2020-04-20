49431587716aaacf2772cd8f19eb4f6ca255ae36
Swapnil Darmora <darmora2@illinois.edu>
2020-04-20 09:43:40 +0530

    adding changes to MP6

--- MP6/target/classes/resources/top_n_finder_bolt.py
+++ MP6/target/classes/resources/top_n_finder_bolt.py
@@ -1,4 +1,4 @@
-import time
+
 import heapq
 from collections import Counter
 
@@ -11,22 +11,19 @@ class WordCountTuple:
         self.word = word
         self.count = count
 
-    def __cmp__(self, o):
-        return self.count > o.count
+    def __cmp__(self, inword):
+        return self.count > inword.count
 
 
 class TopNFinderBolt(storm.BasicBolt):
-    outputs = ['top-N']
 
     def initialize(self, conf, context):
         self._top_words = Counter()
-        self._N = conf.get("mp4.N", 10)
         self._top_N_map = {}
         self._top_N_heap = []
 
     def process(self, tup):
         # TODO:
-        # Task: keep track of the top N words
 
         word = tup.values[0]
         count = int(tup.values[1])
@@ -37,14 +34,11 @@ class TopNFinderBolt(storm.BasicBolt):
             if count > self._top_N_map[word].count:
                 self._top_N_map[word].count = count
                 heapq.heapify(self._top_N_heap)
-                storm.logInfo("Update word: %s, count: %d" % (word, count))
         elif len(self._top_N_heap) < self._N:
             self._top_N_map[word] = new_word_count
             heapq.heappush(self._top_N_heap, new_word_count)
-            storm.logInfo("Add word: %s, count: %d" % (word, count))
         else:
             smallest_word_count = self._top_N_heap[0]
-            storm.logInfo("Current smallest word: %s, count: %d" % (smallest_word_count.word, smallest_word_count.count))
 
             if count > smallest_word_count.count:
                 del(self._top_N_map[smallest_word_count.word])
@@ -52,7 +46,6 @@ class TopNFinderBolt(storm.BasicBolt):
                 heapq.heapreplace(self._top_N_heap, new_word_count)
                 storm.logInfo("Add word: %s, count: %d" % (word, count))
 
-        storm.logInfo("Top N: %s" % self.report())
         storm.emit(["top-N", self.report()])
 
     def report(self):
