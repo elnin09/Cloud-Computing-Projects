91a1e20041d66c0d319269de0d522a62dc201c10
Swapnil Darmora <darmora2@illinois.edu>
2020-04-20 22:43:54 +0530

    adding changes to MP6

--- MP6/target/classes/resources/file_reader_spout.py
+++ MP6/target/classes/resources/file_reader_spout.py
@@ -15,7 +15,7 @@ class FileReaderSpout(storm.Spout):
 
         storm.logInfo("Spout instance starting...")
         #self._myreaderfile = self._conf[input.file] 
-        self._myreadfilepointer = open("/tmp/data.txt")
+        self._myreadfilepointer = open("/mp6/solution/CloudComputing/MP6/data.txt")
         #storm.logInfo("%s",self._myreaderfile)
         #storm.logInfo("see this fucked up thing")
 
